771256f59149fbe07b490c6bed0a5e517d862c71
enzo-han <enzo.han@kakaocorp.com>
2022-07-21 11:45:30 +0900

    [ADD] excel batch

---
+++ src/main/java/com/example/springbatch/ExampleJobConfig.java
@@ -0,0 +1,44 @@
+package com.example.springbatch;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+@EnableBatchProcessing
+@RequiredArgsConstructor
+public class ExampleJobConfig {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+
+    @Bean
+    public Job ExampleJob(){
+        Job exampleJob = jobBuilderFactory.get("exampleJob")
+            .start(Step())
+            .build();
+        return exampleJob;
+    }
+
+    @Bean
+    public Step Step(){
+        return stepBuilderFactory.get("step")
+            .tasklet((contribution, chunkContext) -> {
+                log.info("Step 1");
+                return RepeatStatus.FINISHED;
+            }).build();
+    }
+
+}
