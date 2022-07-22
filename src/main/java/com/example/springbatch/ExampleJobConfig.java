061313bfb9fbef29bb737a58a5b53751d915f067
enzo-han <enzo.han@kakaocorp.com>
2022-07-22 17:27:31 +0900

    [ADD] excel batch

--- src/main/java/com/example/springbatch/ExampleJobConfig.java
+++ src/main/java/com/example/springbatch/ExampleJobConfig.java
@@ -8,15 +8,20 @@ import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.extensions.excel.RowMapper;
+import org.springframework.batch.extensions.excel.poi.PoiItemReader;
+import org.springframework.batch.extensions.excel.support.rowset.RowSet;
+import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
 
 @Slf4j
 @Configuration
-@EnableBatchProcessing
 @RequiredArgsConstructor
 public class ExampleJobConfig {
 
@@ -25,20 +30,46 @@ public class ExampleJobConfig {
 
 
     @Bean
+    @StepScope
+    public PoiItemReader excelReader() {
+        PoiItemReader reader = new PoiItemReader();
+        reader.setResource(new FileSystemResource("./test.xlsx"));
+        System.out.println("test ");
+        reader.setRowMapper(rowMapper());
+        return reader;
+    }
+    @Bean
+    @StepScope
+    public ItemWriter testWriter(){
+        ItemWriter writer = items -> System.out.println("test");
+        return writer;
+    }
+
+    @Bean
+    public RowMapper rowMapper() {
+        return new RowMapper() {
+            @Override
+            public Object mapRow(RowSet rs) throws Exception {
+                System.out.println("rs.getCurrentRow()[0] = " + rs.getCurrentRow()[0]);
+                return rs;
+            }
+        };
+    }
+
+    @Bean
     public Job ExampleJob(){
         Job exampleJob = jobBuilderFactory.get("exampleJob")
-            .start(Step())
+            .start(step())
             .build();
         return exampleJob;
     }
 
     @Bean
-    public Step Step(){
+    public Step step(){
         return stepBuilderFactory.get("step")
-            .tasklet((contribution, chunkContext) -> {
-                log.info("Step 1");
-                return RepeatStatus.FINISHED;
-            }).build();
+            .chunk(1)
+            .reader(excelReader())
+            .writer(testWriter())
+            .build();
     }
-
 }
